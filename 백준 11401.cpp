9972e33f38eee3edb67da5d3070c544cdf5ef80b
Parksangin <sangin0829@gmail.com>
2021-02-08 13:19:41 +0000

    11401 수정

--- 백준 11401.cpp
+++ 백준 11401.cpp
@@ -1,39 +1,49 @@
-#include <cstdio>
-#include <algorithm>
-#define P 1000000007LL
-typedef long long ll;
+#include <iostream>
+#include <set>
 using namespace std;
-ll fac[4000001], n, k, inv[4000001];//inv[x]의 정의는 x의inverse가 아닌 x!의 inverse
-ll power(ll x, ll y) {    //분할 정복을 이용하여 x^y 구하기
-    ll ret = 1;
-    while (y > 0) {
-        if (y % 2) {
-            ret *= x;
-            ret %= P;
-        }
-        x *= x;
-        x %= P;
-        y /= 2;
-    }
-    return ret;
-}
-int main() {
-    fac[1] = 1;
-    for (int i = 1; i <= 4000000; i++)
-        fac[i] = (fac[i - 1] * i) % P;            //factorial 전처리
-    inv[4000000] = power(fac[4000000], P - 2);    //페르마의 소정리를 이용하여 fac(400만)의 inverse를 구함(이때 분할 정복을 이용하여 logP의 시간에 처리) 
-    for (int i = 4000000 - 1; i >= 0; i--) 
-        inv[i] = (inv[i + 1] * (i + 1)) % P;    //inverse(fac(i))=inverse(fac(i+1))*(i+1)이라는 수식을 이용하여 전처리
-    //총 O(N+logP)시간에 전처리를 끝냄 
-    //전처리가 끝났기 때문에 어떤 쿼리가 들어와도 상수시간에 이항 계수를 계산 가능
-    scanf("%lld%lld", &n, &k);
-    if (n == k || !k) {
-        puts("1");
-        return 0;
-    }
-    ll r = (fac[n] * inv[n - k]) % P;
-    r = (r*inv[k]) % P;
-    printf("%lld\n", r);
-    return 0;
-}
+typedef long long ll;
+const int MOD = 1000000007;
 
+int N, K;
+// MOD 가 소수 이므로 (K!)^MOD-1=1(mod MOD), (K!)^(MOD-2) 은 k!의 역원
+// (k!)^(MOD-2) 구하기
+ll Inversion(){
+	ll ret = 1LL;
+	ll temp = 1LL;
+	ll Want = MOD-2;
+	// temp = (K!)^1 = (K!)^(2^0)
+	for(int i=2; i<=K; ++i){
+		temp*=i;
+		temp%=MOD;
+	}
+	// MOD-2 를 이진법으로 계산해서 (K!)^(MOD-2) 게산
+	while(Want){
+		if(Want%2){
+			ret*=temp;
+			ret%=MOD;
+		} 		
+		// (k!)^(2^i) -> (k!)^(2^(i+1))
+		temp*=temp;
+		temp%=MOD;
+		Want/=2;
+	} 
+	return ret%MOD;
+}
+// N! / (N-K)!
+ll Combi(){
+	ll ret = 1LL;
+	for(int i=N; i>N-K; i--){
+		ret*=i;
+		ret%=MOD;
+	}	
+	return ret%MOD;
+}
+int main(){ios::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+
+cin >> N >> K;
+ll a = Inversion();
+ll b = Combi();
+cout << (a*b)%MOD << '\n';
+
+return 0;
+}
